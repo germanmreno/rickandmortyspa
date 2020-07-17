871d21787a9e8320e3a4b0658a6219ae7ce0a078
Germán Moreno <grmmrno@gmail.com>
2020-07-17 16:12:13 -0400

    Fixeando el header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-        <a href="/"><img src="https://lh3.googleusercontent.com/proxy/8YB-30FrxXm_uT2P7WFBifdBfGXOVuIfwL7tdmgFFUuIKDKxs-HnWKC8Gnqc68i5t5kYctJK-HQtscS-VBE6J6eKGz1ORsMoF2w6wsngMgjhtJ0MyXc" alt="Logo" width="250px"/></a>
+        <a href="/rickandmortyspa"><img src="https://lh3.googleusercontent.com/proxy/8YB-30FrxXm_uT2P7WFBifdBfGXOVuIfwL7tdmgFFUuIKDKxs-HnWKC8Gnqc68i5t5kYctJK-HQtscS-VBE6J6eKGz1ORsMoF2w6wsngMgjhtJ0MyXc" alt="Logo" width="250px"/></a>
         </h1>
       </div>
       <div class="Header-nav">
