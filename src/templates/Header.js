b0434b61ce46552a821c911e8cc2e824b19e158b
Germán Moreno <grmmrno@gmail.com>
2020-07-17 15:39:06 -0400

    Proyecto de SPA

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  const view = `
+    <div class="Header-main">
+      <div class="Header-logo">
+        <h1>
+        <a href="/"><img src="https://lh3.googleusercontent.com/proxy/8YB-30FrxXm_uT2P7WFBifdBfGXOVuIfwL7tdmgFFUuIKDKxs-HnWKC8Gnqc68i5t5kYctJK-HQtscS-VBE6J6eKGz1ORsMoF2w6wsngMgjhtJ0MyXc" alt="Logo" width="250px"/></a>
+        </h1>
+      </div>
+      <div class="Header-nav">
+        <a href="#/about/">
+          About
+        </a>
+      </div>
+    </div>
+  `;
+  return view;
+};
+
+export default Header;
