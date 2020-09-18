096fe71aaca864cf5e7296e5a124de7dbbbb794a
Germán Moreno <grmmrno@gmail.com>
2020-09-18 19:54:23 -0400

    Fix header

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-        <a href="/rickandmortyspa"><img src="https://lh3.googleusercontent.com/proxy/8YB-30FrxXm_uT2P7WFBifdBfGXOVuIfwL7tdmgFFUuIKDKxs-HnWKC8Gnqc68i5t5kYctJK-HQtscS-VBE6J6eKGz1ORsMoF2w6wsngMgjhtJ0MyXc" alt="Logo" width="250px"/></a>
+        <a href="/rickandmortyspa"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/1280px-Rick_and_Morty.svg.png" alt="Logo" width="250px"/></a>
         </h1>
       </div>
       <div class="Header-nav">
